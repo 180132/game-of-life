487d1b96ccf8c9da06692c3506b8d47c5185f197
180132 <piotrbed5@wp.pl>
2015-05-06 15:38:35 +0200

    Naprawiony blad 2

--- gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenCreatingANewGame.java
+++ gameoflife-web/src/test/java/com/wakaleo/gameoflife/webtests/controllers/WhenCreatingANewGame.java
@@ -20,34 +20,28 @@ public class WhenCreatingANewGame {
     @Before
     public void initializeController() {
         controller = new GameController();
-    }
-
-    @Test
+    }   @Test
     public void anEmptyUniverseShouldBeAddedToTheSession() {
         ModelAndView homeView = controller.newGame();
-        assertThat(homeView.getModel().get("universee"), is(not(nullValue())));
+       
+        assertThat(homeView.getModel().get("universe"), is(not(nullValue())));
     }
-
+    
     
     @Test
     public void whenTheUserCreatesTheFirstGenerationTheUniverseDimensionsShouldBeAddedToTheSession() {
         HttpServletRequest request = mock(HttpServletRequest.class);
-        
-        ModelAndView homeView = controller.firstGeneration(3, 5, request);
-        ModelAndView homeView = controller.newGame();
-        assertThat(homeView.getModel().get("universe"), is(not(nullValue())));
+
+        ModelAndView homeView = controller.firstGeneration(5, 5, request);
         assertThat((Integer) homeView.getModel().get("rows"), is(3));
         assertThat((Integer) homeView.getModel().get("columns"), is(5));
         
-    }
-    @Test
-    public void whenTheUserCreatesTheFirstGenerationAnEmptyUniverseShouldBeAddedToTheSession() {
         HttpServletRequest request = mock(HttpServletRequest.class);
         ModelAndView homeView = controller.firstGeneration(3, 5, request);
-        
-        assertThat(homeView.getModel().get("universe"), is(not(not(nullValue()))));        
-    }
 
+        assertThat(homeView.getModel().get("universe"), is(not(nullValue())));
+        
+    }
    
 }	
 
